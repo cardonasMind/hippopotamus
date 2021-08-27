244765c1c653208425f5ad7576882f9ea9fd0c6f
Diego Cardona <person.cardonadiego@gmail.com>
2021-08-27 18:30:14 -0500

    Just added https://hippopotamus-client.vercel.app/chat to CORS origin

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.use(cors(corsOptions));*/
 
 app.use((req, res, next) => {
     //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://hippopotamus-client.vercel.app/chat");
     res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
     next();
