fb5e15e5bde2c9cd59c4cd93085447ed1687d93d
Diego Cardona <person.cardonadiego@gmail.com>
2021-08-27 19:01:20 -0500

    Removed CORS

--- server/router.js
+++ server/router.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://hippopotamus-client.vercel.app/");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 router.get('/', (req, res) => {
     res.send("Server is running!");
 });
